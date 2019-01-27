e3a491c3b89e3e47526a92bf13418bc5301e82ca
Michael Szul <michael@szul.us>
2019-01-27 10:05:50 -0500

    Adding more tests

---
+++ test/webservices.test.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+const assert =  require("assert");
+const { expect } = require("chai");
+const { config } = require("dotenv");
+
+config();
+
+describe("Web services tests", () => {
+
+});
